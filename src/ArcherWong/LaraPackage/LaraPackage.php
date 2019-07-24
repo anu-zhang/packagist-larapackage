dfa61ca56589da17a5626401b91e04ee14fa9e95
zhangmengke <michael_u@163.com>
2019-07-24 18:21:23 +0800

    init

---
+++ src/ArcherWong/LaraPackage/LaraPackage.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ArcherWong\LaraPackage\ArcherWong\LaraPackage;
+
+use Illuminate\Support\Facades\Facade;
+
+class LaraPackage extends Facade
+{
+    public static function getFacadeAccessor()
+    {
+        //return 的字符串会在相应的provider中使用
+        return 'larapackage';
+    }
+}
---
+++ src/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ArcherWong\LaraPackage\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\View;
+use Larapackage;
+class IndexController extends BaseController
+{
+    public function index()
+    {
+        //$data = 'Hello World';
+        $data = Larapackage::getInfo();
+        //可以直接使用创建的包里的视图，注意调用是两个冒号
+        return view('LaraPackageView::welcome', ['data' => $data]);
+    }
+}
