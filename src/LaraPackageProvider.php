2d9c6909130dabf8ac492a968b5fcd6912e7bd8b
zhangmengke <michael_u@163.com>
2019-07-24 18:58:39 +0800

    跑通规范化结构后的代码

--- src/LaraPackageProvider.php
+++ src/LaraPackageProvider.php
@@ -15,12 +15,12 @@ class LaraPackageProvider extends ServiceProvider
     public function boot()
     {
         //注册扩展包视图，并起个名字，方便我们调用该文件夹下的视图
-        $this->loadViewsFrom(__DIR__ . '/../../views', 'LaraPackageView');
+        $this->loadViewsFrom(__DIR__ . '/views', 'LaraPackageView');
         //注册扩展包路由，使用php artisan route:list 命令可以查看是否生效
-        $this->loadRoutesFrom(__DIR__ . '/../../routes.php');
+        $this->loadRoutesFrom(__DIR__ . '/routes.php');
         //可以将相应的资源(不仅限于views文件夹)发布到laravel项目的某个位置，下面将views文件夹放到了我们常用的视图文件夹下
         $this->publishes([
-            __DIR__ . '/../../views' => base_path('resources/views/vendor/lara-package'),
+            __DIR__ . '/views' => base_path('resources/views/vendor/lara-package'),
         ]);
     }
 
